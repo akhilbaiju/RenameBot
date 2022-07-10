7174ec7fa12b27800851f785c17e87a8bdfdb023
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2022-07-10 17:46:26 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -4,4 +4,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command(["about"]))
 async def start(client,message):
-	await message.reply_text("Bot :- @rename_urbot\nCreater :- xxx\nLanguage :-Python3\nLibrary :- Pyrogram 1.4.16\nServer :- Heroku")
+	await message.reply_text("<b><i>Onnum Parayan illa.</b></i>")
