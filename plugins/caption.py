0e096fa8a1dba271cc9f18c8ca4312e8e39954a0
Akhil Baiju <45595599+akhilbaiju@users.noreply.github.com>
2022-07-27 14:28:46 +0530

    Create caption.py

---
+++ plugins/caption.py
@@ -0,0 +1,18 @@
+from pyrogram import Client,filters
+ccaption = """\n\n<b><i>⚜️ To Join Click here
+⭐️ @honeybeemovies
+⭐️ @AmazonPrime_Orginal ✅
+⭐️ @honeybeemoviesgroup 
+⭐️ @MalluFlix 🧲
+
+    🅷🅾️🅽🅴🆈 🅱️🅴🅴 🅼🅾️🆅🅸🅴🆂 </b></i>"""
+
+@Client.on_message(filters.group & filters.reply, group=1)
+async def caption(client, message):
+    if message.reply_to_message:
+        ogcap=message.reply_to_message.caption
+        if ogcap==None:
+            newcap=ccaption
+        else:
+            newcap="<b><i>"+str(ogcap)+"</b></i>"+ccaption
+        await message.reply_to_message.copy(message.chat.id, caption=newcap)
